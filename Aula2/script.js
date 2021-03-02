4981c040eb0b58082b28fbedd25a7e25d59fbcac
JEFERSON GOMES CAVALCANTE <74382480+jeferssoncavalcante@users.noreply.github.com>
2021-03-02 16:06:38 -0300

    Update script.js

--- Aula2/script.js
+++ Aula2/script.js
@@ -4,34 +4,34 @@ function verificarCPF(){
     var digito = cpf.charAt(10);
     var regiao = -1;
         switch (digito) {
-            case 0:
+            case "0":
             regiao =  "RS"
                 break;
-            case 1:
+            case "1":
             regiao =  "DF, GO, MT, TO, MS"
                 break;
-            case 2:
+            case "2":
             regiao =  "AM, PA, AP, RR, AC, RO"
                 break;
-            case 3:
+            case "3":
             regiao =  "CE, MA, PI"
                 break;
-            case 4:
+            case "4":
             regiao =  "PB, PE, AL, RN"
                 break;
-            case 5:
+            case "5":
             regiao =  "BA, SE"
                 break;
-            case 6:
+            case "6":
             regiao =  "MG"
                 break;
-            case 7:
+            case "7":
             regiao =  "RJ, ES"
                 break;
-            case 8:
+            case "8":
             regiao =  "SP"
                 break;
-            case 9:
+            case "9":
             regiao =  "SC, PR"
                 break;
             default:
